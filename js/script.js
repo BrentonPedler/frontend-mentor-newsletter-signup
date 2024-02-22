776ec4dcc81a0f820d93983dfd7e7573df6d80a6
BrentonPedler <bpedler@trewknowledge.com>
2024-02-22 14:56:18 -0500

    MVP

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const form = document.querySelector('.newsletter__signup-form');
+	const emailInput = form.querySelector('.newsletter__email-input');
+	const errorMessage = document.querySelector('.newsletter__error-message');
 	let successOverlay;
 
+	// Function to show the success message
 	function showSuccessMessage(email) {
-		const successHTML = `
+			const successHTML = `
 					<div class="newsletter__success-overlay">
 							<div class="newsletter__success">
 									<div class='newsletter__icon-container'>
@@ -18,29 +21,52 @@ document.addEventListener('DOMContentLoaded', function () {
 					</div>
 			`;
 
-		document.body.insertAdjacentHTML('beforeend', successHTML);
-		successOverlay = document.querySelector('.newsletter__success-overlay');
-		document.querySelector('.newsletter__dismiss-btn').addEventListener('click', dismissSuccessMessage);
-		lockScroll();
+			document.body.insertAdjacentHTML('beforeend', successHTML);
+			successOverlay = document.querySelector('.newsletter__success-overlay');
+			document.querySelector('.newsletter__dismiss-btn').addEventListener('click', dismissSuccessMessage);
+			lockScroll();
 	}
 
+	// Lock scrolling
 	function lockScroll() {
-		document.body.style.overflow = 'hidden';
+			document.body.style.overflow = 'hidden';
 	}
 
+	// Unlock scrolling
 	function unlockScroll() {
-		document.body.style.overflow = 'auto';
+			document.body.style.overflow = 'auto';
 	}
 
+	// Dismiss success message and unlock scrolling
 	window.dismissSuccessMessage = function () {
-		successOverlay.remove();
-		unlockScroll();
+			successOverlay.remove();
+			unlockScroll();
 	};
 
-	form.addEventListener('submit', function (event) {
+	// Email validation function
+	function validateEmail(email) {
+			const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; // Simple email validation pattern
+			return regex.test( email );
+	}
+
+	// Form submission event listener
+	form.setAttribute( 'novalidate', '' );
+	form.addEventListener('submit', function ( event ) {
 		event.preventDefault();
-		const emailInput = form.querySelector('.newsletter__email-input');
-		showSuccessMessage(emailInput.value);
-		emailInput.value = '';
+		const emailLabel = form.querySelector( 'label' );
+		if ( validateEmail( emailInput.value ) ) {
+			showSuccessMessage( emailInput.value );
+			emailInput.value = '';
+			emailInput.classList.remove( 'error' );
+			emailLabel.classList.remove( 'error' );
+			errorMessage.style.display = 'none';
+		} else {
+			errorMessage.textContent = 'Valid email required';
+			errorMessage.style.display = 'block';
+			emailInput.classList.add( 'error');
+			emailLabel.classList.add( 'error' );
+		}
 	});
+
 });
+
